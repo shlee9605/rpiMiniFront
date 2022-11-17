8d6b194e68d44cfe0163e5e5fb62d9dd91b84c2d
uLikeSubak <sr05307@gmail.com>
2022-11-17 11:25:52 +0900

    signUp validation

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import App from './App.vue'
 import store from './store'
 import vuetify from './plugins/vuetify'
 
+import * as VeeValidate from 'vee-validate';
+
 new Vue({
   el: '#app',
   router,
   store,
   vuetify,
+  VeeValidate,
   render: h => h(App)
 })
